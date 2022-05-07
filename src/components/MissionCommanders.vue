94f64015451947a61c16228061fa0c93f7492ce6
AndreaCuriel <acurielipn@hotmail.com>
2022-05-07 13:17:50 -0500

    Método getAllMisssionCommanders en MissionCommanders.vue

--- src/components/MissionCommanders.vue
+++ src/components/MissionCommanders.vue
@@ -1,3 +1,52 @@
 <template>
+<div>
+    <h3>Mission Commanders</h3>
+    <div>
+        <ul>
+            <li :class="{ active: index == currentIndex }" v-for="(missionCommander, index) in missionCommanders" :key="index">
+            {{missionCommander.username}}
+
+            </li>
+        </ul>
+
+    </div>
+
+
+</div>
     
 </template>
+
+<script>
+import MissionCommanderService from "./../services/MissionCommanderService";
+
+export default{
+    name: "MissionCommanders",
+    data() {
+        return {
+            missionCommanders: [],
+            currentMissionCommander: [],
+            currentIndex: -1,
+        }
+    },
+
+    methods: {
+        getAllMissionCommanders(){
+            MissionCommanderService.getAll()
+            .then(response => {
+                this.missionCommanders=response.data;
+            })
+            .catch(e => {
+                console.log(e);
+            });
+            
+        }
+    },
+
+    mounted(){
+        this.getAllMissionCommanders();
+    }
+
+
+}
+
+</script>
